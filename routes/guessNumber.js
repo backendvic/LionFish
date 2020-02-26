e06977f7685f0146b2a480e9fbc3c3f102c2b2bb
EC2 Default User <ec2-user@ip-172-31-90-229.ec2.internal>
2020-02-26 23:36:08 +0000

    new new new

---
+++ routes/guessNumber.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+const path = require('path');
+
+router.get('/numberGuess', function(req, res, next) {
+    res.sendFile(path.join(__dirname+'index.html'));
+});
+module.exports=router;
